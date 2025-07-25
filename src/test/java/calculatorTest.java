b7aaba9dfbf3a010a406953b75f3f8d62e822487
Khanyi-Codes <makhubukhanyisile1@gmail.com>
2025-07-25 18:50:29 +0200

    feature(main): Adding unit tests

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.khanyi.Main;
 
 import static org.junit.Assert.assertEquals;
+import static org.khanyi.Main.add;
+import static org.khanyi.Main.calculator;
 
 public class calculatorTest {
     int result ;
@@ -10,8 +12,33 @@ public class calculatorTest {
     @Test
     @DisplayName("Adiition test")
     public void testAdd() {
-        assertEquals(12 , result);
+        float result;
+
+        assertEquals(12 ,calculator("6 + 6"), 1);
     }
+    @Test
+    @DisplayName("Adiition test")
+    public void testSub() {
+        float result;
+
+        assertEquals(0 ,calculator("6 - 6"), 1);
+    }
+    @Test
+    @DisplayName("Adiition test")
+    public void testProduct() {
+        float result;
+
+        assertEquals(12 ,calculator("4 * 3"), 1);
+    }
+    @Test
+    @DisplayName("Adiition test")
+    public void testDiv() {
+        float result;
+
+        assertEquals(12 ,calculator("72 / 6"), 1);
+    }
+
+
 
 
 }
